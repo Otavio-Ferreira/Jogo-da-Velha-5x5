d2bb65cb27a159e031e66b9d9b524124799a5b11
Otavio-Ferreira <otavioferreira4343@gmail.com>
2024-10-09 17:56:38 -0300

    feat: added validation of velha

--- main.c
+++ main.c
@@ -6,11 +6,19 @@
 //que no seu turno coloca uma peça sua no tabuleiro em quealquer espaço vazio. O objetivo do jogo é fazer três peça em sequência: horizontal, veritcal ou diagonal
 //O prgrama deverá ser feito sem a criação de funções e usando matriz. a exibição do tabuleiro (para #define N 5);
 
+// Deseve-se digitar para escolher a posição no seguinte formato (a 1), onde a é a coluna e 1 é a linha, a coluna deve ser uma letra minúscula
+
 #define N 5
 
 int main()
 {
-    int tabuleiro[N][N], linha, condicao = 0, ganhador;
+    if (N < 3 || N > 26) {
+        printf("/////////////////////////////////////////////////////////////////////\n");
+        printf("///// Tamanho de tabuleiro inválido inválido, tente novamente. //////\n");
+        printf("/////////////////////////////////////////////////////////////////////\n\n");
+        return 1;
+    }
+    int tabuleiro[N][N], linha, condicao = 0, ganhador, velha = 0;
     int jogador = 1;
     char coluna;
     setlocale(LC_ALL, "portuguese");
@@ -22,13 +30,13 @@ int main()
         }
     }
 
-    printf("Bem vindo ao jogo da velha maluca 5x5... Esse é o tabuleiro do jogo... \n\n\n");
+    printf("Bem vindo ao jogo da velha maluca %ix%i... Esse é o tabuleiro do jogo... \n\n\n", N, N);
 
     while(condicao == 0){
         for(int c = 0; c < N; c++){
 
             for(int l = 0; l < N; l++){
-                if(l == 4){
+                if(l == N -1){
                     printf("+---+");
                 }
                 else{
@@ -37,7 +45,7 @@ int main()
             }
             printf("\n");
             for(int l = 0; l < N; l++){
-                if(l == 4){
+                if(l == N -1){
                     if(tabuleiro[c][l] == 0){
                         printf("|   |");
                         printf(" %i ", c+1);
@@ -67,7 +75,7 @@ int main()
         }
 
         for(int l = 0; l < N; l++){
-            if(l == 4){
+            if(l == N -1){
                 printf("+---+");
             }
             else{
@@ -76,7 +84,7 @@ int main()
         }
         printf("\n");
         for(int l = 0; l < N; l++){
-            if(l == 4){
+            if(l == N -1){
                 printf("  %c  ", l + 97);
             }
             else{
@@ -93,12 +101,16 @@ int main()
 
         linha--;
         int colunaIndex = (int)(coluna - 'a');
-        system("clear");
+        system("cls");
 
         if (linha >= 0 && linha < N && colunaIndex >= 0 && colunaIndex < N) {
             if (tabuleiro[linha][colunaIndex] == 0) {
                 tabuleiro[linha][colunaIndex] = jogador;
                 jogador = (jogador == 1) ? 2 : 1;
+                velha++;
+                if(velha >= (N*N)){
+                    condicao = 1;
+                }
             } else {
                 printf("//////////////////////////////////////////////////////////\n");
                 printf("///// Essa posição já está ocupada. Tente novamente. /////\n");
@@ -115,39 +127,44 @@ int main()
                 if(tabuleiro[l][j] != 0){
                     int jogador_marcado = tabuleiro[l][j];
 
-                    // Verifica horizontalmente
                     if(j + 2 < N && tabuleiro[l][j] == jogador_marcado && tabuleiro[l][j+1] == jogador_marcado && tabuleiro[l][j+2] == jogador_marcado){
                         ganhador = jogador_marcado;
                         condicao = 1;
                     }
 
-                    // Verifica verticalmente
                     if(l + 2 < N && tabuleiro[l][j] == jogador_marcado && tabuleiro[l+1][j] == jogador_marcado && tabuleiro[l+2][j] == jogador_marcado){
                         ganhador = jogador_marcado;
                         condicao = 1;
                     }
 
-                    // Verifica diagonal principal
                     if(l + 2 < N && j + 2 < N && tabuleiro[l][j] == jogador_marcado && tabuleiro[l+1][j+1] == jogador_marcado && tabuleiro[l+2][j+2] == jogador_marcado){
                         ganhador = jogador_marcado;
                         condicao = 1;
                     }
 
-                    // Verifica diagonal secundária
                     if(l + 2 < N && j - 2 >= 0 && tabuleiro[l][j] == jogador_marcado && tabuleiro[l+1][j-1] == jogador_marcado && tabuleiro[l+2][j-2] == jogador_marcado){
                         ganhador = jogador_marcado;
                         condicao = 1;
                     }
+
                 }
             }
         }
     }
 
-    system("clear");
+    system("cls");
 
-    printf("///////////////////////////////////////\n");
-    printf("///// O ganhador foi o jogador %i /////\n", ganhador);
-    printf("///////////////////////////////////////\n\n");
+    if(velha >= (N*N)){
+        printf("//////////////////\n");
+        printf("///// Empate /////\n", ganhador);
+        printf("//////////////////\n\n");
+
+    }
+    else{
+        printf("///////////////////////////////////////\n");
+        printf("///// O ganhador foi o jogador %i /////\n", ganhador);
+        printf("///////////////////////////////////////\n\n");
+    }
 
     return 0;
 }
